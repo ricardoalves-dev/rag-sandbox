0ee6fefb42528e83ea05246004b77a82028a3e0d
TEKSYSTEM\Ricardo Alves <ricardoalves@teksystem.com.br>
2025-03-26 17:19:45 -0300

    Gerar resposta

--- lib/ai/embedding.ts
+++ lib/ai/embedding.ts
@@ -1,14 +1,16 @@
-import { embedMany } from 'ai';
+import { embed, embedMany } from 'ai';
 import { openai } from '@ai-sdk/openai';
-
-interface IEmbedding {
-    embedding: number[],
-    content: string,
-}
+import { db } from '../db';
+import { cosineDistance, desc, gt, sql } from 'drizzle-orm';
+import { embeddings } from '../db/schema/embeddings';
 
 const embeddingModel = openai.embedding('text-embedding-3-small');
 
 const generateChunks = (input: string): string[] => {
+    console.log(input
+        .trim()
+        .split('.')
+        .filter(i => i !== ''));
     return input
         .trim()
         .split('.')
@@ -17,11 +19,35 @@ const generateChunks = (input: string): string[] => {
 
 export const generateEmbeddings = async (
     value: string,
-): Promise<Array<IEmbedding>> => {
+): Promise<Array<{ embedding: number[]; content: string }>> => {
     const chunks = generateChunks(value);
     const { embeddings } = await embedMany({
         model: embeddingModel,
         values: chunks,
     });
     return embeddings.map((e, i) => ({ content: chunks[i], embedding: e }));
+};
+
+export const generateEmbedding = async (value: string): Promise<number[]> => {
+    const input = value.replaceAll('\\n', ' ');
+    const { embedding } = await embed({
+        model: embeddingModel,
+        value: input,
+    });
+    return embedding;
+};
+
+export const findRelevantContent = async (userQuery: string) => {
+    const userQueryEmbedded = await generateEmbedding(userQuery);
+    const similarity = sql<number>`1 - (${cosineDistance(
+        embeddings.embedding,
+        userQueryEmbedded,
+    )})`;
+    const similarGuides = await db
+        .select({ name: embeddings.content, similarity })
+        .from(embeddings)
+        .where(gt(similarity, 0.5))
+        .orderBy(t => desc(t.similarity))
+        .limit(4);
+    return similarGuides;
 };
